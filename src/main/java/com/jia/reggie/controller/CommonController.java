9155a1405de123ce1effb67ebbffdf0774393a1f
jiayaoyi <jiayaoyijp@outlook.com>
2023-07-11 06:43:16 +0900

    feat:test

---
+++ src/main/java/com/jia/reggie/controller/CommonController.java
@@ -0,0 +1,75 @@
+package com.jia.reggie.controller;
+
+import com.jia.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 文件上传下载相关处理
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+
+    @Value("${reggie.path}")
+    private String basePath;
+
+    /**
+     * 上传图片方法
+     *
+     * @param multipartFile 文件
+     * @return 消息
+     */
+    @PostMapping("/upload")
+    public R<String> upload(@RequestPart("file") MultipartFile multipartFile) {
+        String originalFilename = multipartFile.getOriginalFilename();
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String fileName = uuid + suffix;
+        File dir = new File(basePath);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        try {
+            multipartFile.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return R.success(fileName);
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param name     文件名
+     * @param response 响应
+     */
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            byte[] bytes = new byte[1024];
+            int length = 0;
+            while ((length = fileInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, length);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/jia/reggie/mock/FileTest.java
@@ -0,0 +1,16 @@
+package com.jia.reggie.mock;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+public class FileTest {
+    @Test
+    public void test(){
+        String originalFilename = "sfdadsf.jpg";
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String fileName = uuid+suffix;
+        System.out.println(fileName);
+    }
+}
